a6627e17e820c1cb39fa9f7322b4fdfe9cf1d004
dpfewell <dpfewell@gmail.com>
2020-01-14 15:20:41 -0800

    Finished exercises

---
+++ chapter-05-exercises/exercise-1/practice.R
@@ -0,0 +1,16 @@
+# my first program
+print("Hello World!")
+
+x <- 4 # x refers to 4
+y <- x # y refers to x (4)
+z <- y + 7 # z refers to the sum of y and 7 (11)
+
+# valid program, but what does this mean?
+a <- 35.0
+b <- 12.50
+c <- a * b
+
+#this one is better
+hours <- 35.0
+pay_rate <- 12.50
+earnings <- hours * pay_rate
